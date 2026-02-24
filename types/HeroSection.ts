4d9a2283d02834beea898d0f69013cdb2ee5601d
khalid chouhan <khalidchuhan7886@gmail.com>
2026-02-24 21:21:01 +0500

    updates

--- types/HeroSection.ts
+++ types/HeroSection.ts
@@ -7,7 +7,7 @@ export interface DetailedSkill {
   name: string;
   percentage: number;
 }
-export type SkillIcon = "Server" | "Laptop" | "Brain" | "Code";
+export type SkillIcon = "Server" | "Laptop" | "Brain" | "Code" | "Cloud" | "Database" | "Wrench" | "TestTube";
 
 export interface SkillCategory {
   title: string;
